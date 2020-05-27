74757192c1ce53636b6eeb4081cbd58e670051dc
Ali Nazari <ali.nazari@aut.ac.ir>
2020-05-27 11:17:57 +0430

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-cd /opt/certbot/
+apt-get update apt-get install python-minimal python --version apt-get install git-core git --version
+cd /opt git clone https://github.com/certbot/certbot.git
 
+cd /opt/certbot/
 ./certbot-auto certonly --manual --preferred-challenges=dns --email ali.nazari.porfosor2@gmail.com --server https://acme-v02.api.letsencrypt.org/directory --agree-tos -d *.ali-nazari.ir -d ali-nazari.ir
